eb11a4720ecd4f31e00b082d553f160409661d05
narratorxu <2693665523@qq.com>
2026-05-11 01:02:49 +0800

    S019. Input 轮询

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -5,6 +5,8 @@
 
 #include <glad/glad.h>
 
+#include "Hazel/Input.h"
+
 namespace Hazel
 {
 
@@ -64,6 +66,9 @@ namespace Hazel
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
+
+			auto [x, y] = Input::GetMousePosition();
+			HZ_CORE_TRACE("{0}, {1}", x, y);
 
 			m_Window->OnUpdate();
 
